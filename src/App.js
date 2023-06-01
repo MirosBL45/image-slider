4fc5972580d9c2978374339ec283f4b551d989ec
Miroslav Jovic <jovicmiroslav88@gmail.com>
2023-06-02 00:10:43 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { motion, useAnimation } from 'framer-motion';
+import images from './images';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
+  const [width, setWidth] = useState(0);
+  const carousel = useRef();
+  const controls = useAnimation();
+
+  useEffect(() => {
+    console.log(carousel.current.scrollWidth, carousel.current.offsetWidth);
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+  }, []);
+
+  const handlePrev = () => {
+    controls.start({ x: 0 });
+  };
+
+  const handleNext = () => {
+    controls.start({ x: -width });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <motion.div ref={carousel} className="carousel">
+        <motion.div
+          drag="x"
+          dragConstraints={{ right: 0, left: -width }}
+          whileTap={{ cursor: 'grabbing' }}
+          className="inner-carousel"
+          animate={controls}
         >
-          Learn React
-        </a>
-      </header>
+          {images.map((image, index) => (
+            <motion.div className="item" key={index}>
+              <img src={image} alt={`cat ${index + 1}`} />
+            </motion.div>
+          ))}
+        </motion.div>
+      </motion.div>
+      <div className="controls">
+        <button onClick={handlePrev}>Start</button>
+        <button onClick={handleNext}>End</button>
+      </div>
     </div>
   );
 }
